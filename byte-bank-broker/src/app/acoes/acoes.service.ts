eeaf9b87e21a537588fd7a2fe652cf9d0881fc59
Marcelo Souza <marcelo.tadeu@mjv.com.br>
2021-06-01 18:58:29 -0300

    adiciona projeto no github

---
+++ byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map, pluck, tap } from 'rxjs/operators';
+import { Acao, AcoesAPI } from './modelo/acoes';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AcoesService {
+  constructor(private httpClient: HttpClient) {}
+
+  getAcoes(valor?: string) {
+    const params = valor ? new HttpParams().append('valor', valor) : undefined;
+    return this.httpClient
+      .get<AcoesAPI>('http://localhost:3000/acoes', { params })
+      .pipe(
+        tap((valor) => console.log(valor)),
+        pluck('payload'),
+        // ou map((api) => api.payload),
+        map((acoes) =>
+          acoes.sort((acaoA, acaoB) => this.ordenaPorCodigo(acaoA, acaoB))
+        )
+      );
+  }
+
+  private ordenaPorCodigo(acaoA: Acao, acaoB: Acao) {
+    if (acaoA.codigo > acaoB.codigo) {
+      return 1;
+    } else if (acaoA.codigo < acaoB.codigo) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
+}
